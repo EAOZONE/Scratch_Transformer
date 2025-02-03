59dfdecab4595fce88aeb1760540586b07496019
Ben <bpratt@ihmc.org>
2025-02-03 17:45:25 -0500

    target is no longer zero

--- trainingAI.py
+++ trainingAI.py
@@ -1,7 +1,8 @@
+import os
 import random
 import torch
 from torch import nn, autocast
-from torch.utils.data import random_split, DataLoader, Subset
+from torch.utils.data import random_split, DataLoader, Subset, Dataset
 from torchvision.datasets import DatasetFolder
 from torch.optim import Adam
 from torch.cuda.amp import GradScaler
@@ -19,6 +20,28 @@ EPOCHS = 100
 REDUCED_RATIO = 0.2
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+class CustomDataset(Dataset):
+    def __init__(self, data_list, target_list):
+        self.data_list = data_list
+        self.target_list = target_list
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, idx):
+        data = torch.load(self.data_list[idx])
+        target = torch.load(self.target_list[idx])
+        data = data.permute(0, 3, 1, 2)  # Permute dimensions to [B, C, H, W]
+        return data, target
+
+def get_random_files(folder, num_files):
+    all_files = [f for f in os.listdir(folder) if f.endswith('.pth')]
+    selected_files = random.sample(all_files, num_files)
+    return [os.path.join(folder, f) for f in selected_files]
+
+
+
+
 def validate_patch_size(img_size, patch_size):
     """Validate that img_size is divisible by patch_size."""
     if any(img_size[i] % patch_size[i] != 0 for i in range(2)):
@@ -28,44 +51,19 @@ def remove_batch_channel(x):
     """Remove extra batch channel."""
     return x.squeeze(0)
 
-def tensor_transform(tensor):
-    """Global function to normalize tensors."""
-    return (tensor - 0.5) / 0.5
-
-
-def preprocess_data():
-    """Return the global transform function."""
-    return tensor_transform
-
-def visualize_image(data, title="Image"):
-    """Visualize the data using Matplotlib."""
-    data_to_plot = data[:3, :, :]
-    data_to_plot = data_to_plot.transpose(1, 2, 0)
-    plt.imshow(data_to_plot)
-    plt.title(title)
-    plt.show()
-
-def file_loader(filepath):
-    """file loader function for PyTorch .pth tensor files."""
-    data = torch.load(filepath, weights_only=False)
-    return data
-
 def main():
     validate_patch_size(IMG_SIZE, PATCH_SIZE)
 
-    # Load the dataset
-    dataset = DatasetFolder(
-        root='./sample_data/',
-        loader=file_loader,
-        extensions=('.pth',),
-        transform=preprocess_data()
-    )
+    # Set the folder path and number of files to select
+    folder_path = 'sample_data/2d'
+    num_files = 50
 
-    # Get datasets for training, validation, and testing
-    test_size = int(0.2 * len(dataset))
-    input_size = 5
-    train_size = len(dataset) - test_size - input_size
-    train_dataset, test_dataset, sample_input = random_split(dataset, [train_size, test_size, input_size])
+    # Get random files for data and target lists
+    data_list = get_random_files(folder_path, num_files)
+    target_list = get_random_files(folder_path, num_files)
+
+    dataset = CustomDataset(data_list, target_list)
+    data_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
     # Initialize model
     model = MyVIT2D(
@@ -82,23 +80,17 @@ def main():
     criterion = nn.MSELoss()
     scaler = GradScaler()
 
-    all_train_indices = list(range(len(train_dataset)))
-    sampled_indices = random.sample(all_train_indices, int(len(train_dataset) * REDUCED_RATIO))
-    sampler = torch.utils.data.SubsetRandomSampler(sampled_indices)
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, sampler=sampler, num_workers=4, pin_memory=True)
-
 
     for epoch in range(EPOCHS):
-        train_loader.sampler.indices = random.sample(all_train_indices, int(len(train_dataset) * REDUCED_RATIO))
         # Trains model on each batch
         model.train()
         optimizer.zero_grad()
-        for i, (data, target) in enumerate(train_loader):
+        for i, (data, target) in enumerate(data_loader):
             B, _, H, W, D = data.shape
-            data = data.permute(0, 4, 1, 2, 3).reshape(B, D, H, W)
+            data = data.permute(0, 4, 1, 2, 3).reshape(B, H, W, D)
+            B, _, H, W, D = target.shape
+            target = target.permute(0, 4, 1, 2, 3).reshape(D, H, W, B)
             data, target = data.to(DEVICE), target.to(DEVICE, dtype=torch.float32)
-
-            target = target.view(-1, 1)
 
             with autocast(device_type='cuda', dtype=torch.float16):
                 output = model(data)
